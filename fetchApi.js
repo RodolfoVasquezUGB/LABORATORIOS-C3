c25731b9ab4fe4de21bc9433831ed105bb011469
RodolfoVasquezUGB <98406990+RodolfoVasquezUGB@users.noreply.github.com>
2022-05-03 14:41:01 -0600

    Update fetchApi.js

--- fetchApi.js
+++ fetchApi.js
@@ -1,6 +1,6 @@
 const fetch = require ('node-fetch')
 
-fetch ('https://api.coindesk.com/v1/bpi/currentprice.json')
+/*fetch ('https://api.coindesk.com/v1/bpi/currentprice.json')
     .then((response) => {
         return response.json();
     })
@@ -10,4 +10,27 @@ fetch ('https://api.coindesk.com/v1/bpi/currentprice.json')
     
     .catch((error) => {
         console.log(error)
-    })
+    })*/
+
+    async function getCoin() {
+        try {
+            const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+            const data = await response.json();
+            let BitcoinList = "";
+            Array.forEach(bitcoin => {
+                BitcoinList += `${bitcoin['name']} `;
+                
+            });
+            //crear archivo
+            fs.writeFile('bitcoin.csv', BitcoinList,(error) => {
+                if(error){
+                    console.log(error);
+                    return;
+                }
+                console.log('se ha creado el archivo');
+            })
+        }catch(error){
+            console.log(error)
+        }
+    }
+getCoin();
